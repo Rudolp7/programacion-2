524193cd70237d57e7e3c975571961198def64ad
Rudolp7 <112682101+Rudolp7@users.noreply.github.com>
2022-10-20 21:30:34 -0500

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.awt.Color;
+
+class Snake {
+    private Color color;
+    private ArrayList<Point> body = new ArrayList<Point>();
+    private int speed;
+
+    public Snake() {
+        body.add(new Point(5, 2));
+        body.add(new Point(4, 2));
+        body.add(new Point(3, 2));
+        color = Color.BLUE;
+        speed = 1;
+    }
+
+    public ArrayList<Point> getBody() {
+        return body;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
